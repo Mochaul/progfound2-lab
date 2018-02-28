6e245f331dc88f4e829f3bbfd987a65e1392a074
Nicolaus <nicolausgozali@gmail.com>
2018-02-28 23:10:48 +0700

    add short description of class methods

--- lab_4/src/main/java/Manusia.java
+++ lab_4/src/main/java/Manusia.java
@@ -5,6 +5,7 @@ public class Manusia {
     private int umur;
     private int uang = 50000;
     private double kebahagiaan = 50.0;
+    private boolean hidup;
 
     // CONSTRUCTOR //
     public Manusia(String nama, int umur){
@@ -60,16 +61,17 @@ public class Manusia {
          * if sufficient, both instance's happiness increase
          */
         int amount = 0;
-        for(int i = 0; i < man.getNama().length(); i++){    // calculate amount if not given
+        for(int i = 0; i < man.getNama().length(); i++){
             amount += (int)man.getNama().charAt(i);
         }
-        amount *= 100;
+        amount *= 100; // calculate amount given
 
         if (this.getUang() >= amount){
-            this.setUang(this.getUang() - amount); // deduct cash from sender
-            man.setUang(man.getUang() + amount);   // add cash to receiver
+            // update sender and receiver cash if sufficient
+            this.setUang(this.getUang() - amount);
+            man.setUang(man.getUang() + amount);
 
-            // update sender and recipient happiness and adjusts it if out of range
+            // update sender and receiver happiness and adjusts it if out of range
             this.setKebahagiaan(this.getKebahagiaan() + amount/6000.0);
             this.adjustKebahagiaan();
             man.setKebahagiaan(this.getKebahagiaan() + amount/6000.0);
@@ -82,11 +84,17 @@ public class Manusia {
 
     }
     public void beriUang(Manusia man, int amount){
+        /**
+         * gives money to another instance of Manusia
+         * amount going to be given as an argument
+         * if sufficient, both instance's happiness increase
+         */
         if (this.getUang() >= amount){
-            this.setUang(this.getUang() - amount); // deduct cash from giver
-            man.setUang(man.getUang() + amount);   // add cash to receiver
+            // update sender and receiver cash if sufficient
+            this.setUang(this.getUang() - amount);
+            man.setUang(man.getUang() + amount);
 
-            // update sender and recipient kebahagiaan and adjusts it if out of range
+            // update sender and receiver happiness and adjusts it if out of range
             this.setKebahagiaan(this.getKebahagiaan() + amount/6000.0);
             this.adjustKebahagiaan();
             man.setKebahagiaan(this.getKebahagiaan() + amount/6000.0);
@@ -98,29 +106,40 @@ public class Manusia {
         }
     }
     public void bekerja(int durasi, int bebanKerja){
-        if (this.getUmur() < 18 ){
+        /**
+         * increases cash and decreases happiness by working
+         */
+        if (this.getUmur() < 18 ){ // cannot work if age is less than 18
             System.out.println(this.getNama() + " belum boleh bekerja karena masih dibawah umur D:");
             return;
         }
+
         int bebanKerjaTotal = durasi * bebanKerja;
         int pendapatan;
+
+        // decrease and adjust happiness if sufficient for work
         if (bebanKerjaTotal <= this.getKebahagiaan()){
-            this.setKebahagiaan(this.getKebahagiaan() - bebanKerjaTotal); // decrease and adjust kebahagiaan
+            this.setKebahagiaan(this.getKebahagiaan() - bebanKerjaTotal); 
             this.adjustKebahagiaan();
             pendapatan = bebanKerjaTotal * 10000;
             System.out.println(this.getNama() + " bekerja full time, total pendapatan : " + pendapatan);
-        }else{
-            int DurasiBaru = (int)(this.getKebahagiaan() / bebanKerja); // if kebahagiaan insufficient, duration of work is recalculated
+        }
+        // if happiness insufficient, duration of work is recalculated
+        else{
+            int DurasiBaru = (int)(this.getKebahagiaan() / bebanKerja); 
             bebanKerjaTotal = DurasiBaru * bebanKerja;
             pendapatan = bebanKerjaTotal * 10000;
-            this.setKebahagiaan(this.getKebahagiaan() - bebanKerjaTotal); // decrease and adjust kebahagiaan
+            this.setKebahagiaan(this.getKebahagiaan() - bebanKerjaTotal);
             this.adjustKebahagiaan();
             System.out.println(this.getNama() + " tidak bekerja secara full time karena sudah terlalu lelah, total pendapatan : " + pendapatan);
         }
-        
-        this.setUang(this.getUang() + pendapatan); // update cash after working
+        // update cash after working
+        this.setUang(this.getUang() + pendapatan); 
     }
     public void rekreasi(String namaTempat){
+        /**
+         * increases happiness but reduces cash by going to recreation place
+         */
         int biaya = namaTempat.length() * 10000;
 
         if (this.getUang() >= biaya){
@@ -133,6 +152,9 @@ public class Manusia {
         }
     }
     public void sakit(String namaPenyakit){
+        /**
+         * decreases happiness because of sickness
+         */
         this.setKebahagiaan(this.getKebahagiaan() - namaPenyakit.length());
         this.adjustKebahagiaan();
         System.out.println(this.getNama() + " terkena penyakit " + namaPenyakit + " :O");
@@ -156,7 +178,7 @@ public class Manusia {
     private void adjustKebahagiaan(){
         /**
          * adjust kebahagiaan to 100.0 (max) or 0.0 (min)
-         * if it is out of range 0.0 < x < 100.0
+         * if it is out of the range 0.0 < x < 100.0
          */
         if(this.getKebahagiaan() > 100.0){
             this.setKebahagiaan(100.0);
